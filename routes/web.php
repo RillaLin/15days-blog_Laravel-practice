22e094e0b2dc7a8c2dbc7cf1305d08376873633e
RillaLin <72490355+RillaLin@users.noreply.github.com>
2021-03-06 15:52:30 +0800

    *init*

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//看文章用get就可以了
+Route::get('/', function () {   //首頁
+    return view('index');
+});
+
+Route::get('/about', function () { //關於
+    return view('about');
+});
+
+Route::get('/contact', function () {  //contact
+    return view('contact');
+});
+
+Route::get('/posts', function () {  //顯示所有文章的列表
+    return view('posts.list');  
+});
+
+Route::get('/posts/{id}', function ($id) {  //顯示單篇文章
+    return view('posts.show');
+});
