178c330fe5ca2d6e6fe2f459e3701a64bcc9579c
RillaLin <72490355+RillaLin@users.noreply.github.com>
2021-03-16 23:02:31 +0800

    +posts table

---
+++ database/migrations/2021_03_13_132007_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade'); //刪使用者時順便把他的文章都刪除
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts',function(Blueprint $table){ //把table叫進來
+            $table->dropForeign(['user_id']);            //把FK丟掉
+        });
+
+        Schema::dropIfExists('posts');
+    }
+}
