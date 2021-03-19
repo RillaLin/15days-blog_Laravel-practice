8dcfa93080b3849cf323f391b9298c47412ae3dd
RillaLin <72490355+RillaLin@users.noreply.github.com>
2021-03-19 22:51:41 +0800

    +fillable columns

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['title','content'];  //設定可寫入資料的欄位
+
     public function user()   //和user model做串聯，一篇文章對到一個user
     {
         return $this->belongsTo('App/User');  //post屬於user
