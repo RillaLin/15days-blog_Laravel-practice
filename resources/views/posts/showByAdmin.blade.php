f113e22ce7cdad268547ae9f8a0d8120a60b3047
RillaLin <72490355+RillaLin@users.noreply.github.com>
2021-03-19 23:41:58 +0800

    admin show view +2 buttons(edit/delete)

--- resources/views/posts/showByAdmin.blade.php
+++ resources/views/posts/showByAdmin.blade.php
@@ -10,7 +10,7 @@
                     <ol class="breadcrumb">
                         <li><a href="/">Home</a>
                         </li>
-                        <li class="active"><a href="/posts/admin">Blog</a>
+                        <li class="Blog Admin Panel"><a href="/posts/admin">Blog</a>
                         </li>
                         <li class="active">Blog Single</li>
                     </ol>
@@ -26,6 +26,10 @@
     <div class="page-content">
         <div class="container">
             <h1>{{ $post -> title }}</h1>
+            <div class="toolbox">
+                <a href="#" class="btn btn-primary">Edit</a>
+                <button class="btn btn-danger">Delete</button>
+            </div>
             <div class="content">
                 {{ $post -> content }}
             </div>
