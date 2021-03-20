f2f61d6e07b5c0bee537045ceaf93513a098d9c0
RillaLin <72490355+RillaLin@users.noreply.github.com>
2021-03-20 23:01:30 +0800

    +migration
    +user_id to posts table
    link posts table to users table

---
+++ database/migrations/2021_03_20_140257_add_user_to_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedInteger('user_id')->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade'); //刪使用者時順便把他的文章都刪除
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+
+            $table->dropColumn('user_id');
+        });
+    }
+}
