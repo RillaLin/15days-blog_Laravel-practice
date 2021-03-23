8cd1a5e7b32a13d9d5c990cf5dcd31fbc45378f7
RillaLin <72490355+RillaLin@users.noreply.github.com>
2021-03-24 00:08:16 +0800

    +category index

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('page-title')
+    <!--page title start-->
+    <section class="page-title">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="text-uppercase">Category Admin Panel</h4>
+                    <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Category Admin Panel</li>
+                    </ol>
+</nav>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--page title end-->
+
+@endsection
+
+@section('content')
+    <section class="body-content ">
+
+        <div class="page-content">
+            <div class="container">
+                <div class="clearfix toolbox">  <!--預防靠右浮動出畫面外-->
+                    <a href="/categories/create" class="btn btn-primary pull-right">creat category</a>  <!--新建按鈕，連結到create表單的頁面-->
+                </div>
+                
+                <!--使用bootstrate 3.3.7版本bootstrate的group list-->
+                <ul class="list-group">
+                    @foreach($categories as $key=> $category) <!--把每一個post拿出來印-->
+                        <li class="list-group-item clearfix"> <!--改為ul，a裡面放a會出錯-->
+                            <div class="float-left">
+                                <div class="title">{{$category->name}}</div>
+                            </div>
+                            
+                            <div class="float-right">  <!--浮右-->
+                                <a href="/categories/{{ $category->id }}/edit" class="btn btn-primary">Edit</a>
+                                <button class="btn btn-danger" onclick="deleteCategory({{$category->id}})">Delete</button> <!--把後端id塞到前端-->
+                            </div>
+                        </li>
+                    @endforeach
+                    
+                    
+                </ul>
+                
+            </div>
+        </div>
+
+
+    </section>
+
+    <form id="delete-form" action="/posts/id" method="post">
+        <input type="hidden" name="_method" value="delete">
+        @csrf
+    </form>
+
+@endsection
+
